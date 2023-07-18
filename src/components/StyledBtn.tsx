ae216d7413a6c98b098bb7d63d617dea49d13d0e
YuraSS23 <nikitsenko_yura@mail.ru>
2023-07-18 10:27:36 +0300

    v1

--- src/components/StyledBtn.tsx
+++ src/components/StyledBtn.tsx
@@ -10,9 +10,6 @@ export const StyledBtn = styled.button<StyledBtn>`
   border-radius: 5px;
   margin: 10px 0px 0px 20px;
   border: 2px solid #4E71FE;
-
-  
   color: ${props => props.color || "#3f65fb"};
   background-color: ${props => props.background || "#3f65fb"};
-  
 `
